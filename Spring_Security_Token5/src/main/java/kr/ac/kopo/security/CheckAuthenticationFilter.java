f28188fa746fc5a4280b127df87d2899c00594b3
dj3129-bit <dleowns3129@gmail.com>
2025-11-17 15:53:34 +0900

    Add files via upload

--- Spring_Security_Token5/src/main/java/kr/ac/kopo/security/CheckAuthenticationFilter.java
+++ Spring_Security_Token5/src/main/java/kr/ac/kopo/security/CheckAuthenticationFilter.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import jakarta.servlet.FilterChain;
@@ -24,6 +26,12 @@ public class CheckAuthenticationFilter extends BasicAuthenticationFilter {
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		log.debug("Authorization: " + request.getHeader("Authorization"));
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if(authentication == null)
+			log.debug("로그인 정보 없음");
+		else
+			log.debug("권한: " + authentication.getAuthorities());
 		
 		super.doFilterInternal(request, response, chain);
 	}
--- Spring_Security_Token5/src/main/java/kr/ac/kopo/security/SecurityConfig.java
+++ Spring_Security_Token5/src/main/java/kr/ac/kopo/security/SecurityConfig.java
@@ -1,8 +1,13 @@
 package kr.ac.kopo.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +15,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+				
+		authenticationProvider.setPasswordEncoder(passwordEncoder);
+		
+				
+		return authenticationProvider;
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(List<AuthenticationProvider> authenticationProviders) {
+		return new ProviderManager(authenticationProviders);
+	}
 	
 	@Bean
 	CheckAuthenticationFilter checkAuthenticationFilter(AuthenticationManager authenticationManager) {
